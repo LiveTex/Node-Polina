490ef58cc71b18bfcc8d729d4810af2235e00511
Igor Korshunov <igorkorshynov@gmail.com>
2014-02-28 12:17:31 +0400

    Replace IntValue,StrVlue and ErrVlue parsers on SimpleValue parser

--- etc/build/index.d
+++ etc/build/index.d
@@ -21,9 +21,8 @@ polina/redis/script-invoke.js
 
 polina/redis/resp/resp.js
 polina/redis/resp/i-value.js
-polina/redis/resp/string-value.js
 polina/redis/resp/bulk-value.js
-polina/redis/resp/error-value.js
-polina/redis/resp/int-value.js
 polina/redis/resp/arr-value.js
+polina/redis/resp/simple-value.js
 polina/redis/resp/value-type.js
+polina/redis/resp/redis-type.js
--- test/commands.js
+++ test/commands.js
@@ -6,8 +6,7 @@ var client = new polina.redis.Client(6379);
 client.set('be cool', '123', console.info, console.error);
 client.get('be cool', console.info, console.error);
 
-
-//client.sadd('be cool', 'при вет', console.info, console.error);
+client.sadd('be cool', 'при вет', console.info, console.error);
 //client.sadd('me', 'кононе нко', console.info, console.error);
 //client.sadd('me', ',', console.info, console.error);
 client.lrange('arr','0','-1', console.info, console.error);
