7e37d8d9fecc95655c5cc54e2402792adf687b30
Valeria Lepina <divergence082@gmail.com>
2014-07-02 17:09:21 +0400

    redis ok

---
+++ benchmarks/redis/redis-polina.js
@@ -0,0 +1,65 @@
+
+
+var polina = require('../../bin');
+
+var count = 0;
+var step = 1;
+
+var r = 0;
+var e = 0;
+var t = Date.now();
+var mem = 0;
+
+var data = (new Array((1024*10)+1)).join(' ');
+//var data = 'my_data';
+var client = new polina.redis.Client(6379);
+
+
+function complete() {
+  mem += process.memoryUsage().heapUsed/1024/1024;
+
+  if ((r += 1) === count) {
+    console.log('[REDIS-POLINA] | R:', r, ' | E:', e,
+        ' | T:', Date.now() - t, ' | M:', (Math.round(mem/r*10)/10));
+    run();
+  }
+}
+
+
+function cancel() {
+  e += 1;
+  complete();
+}
+
+
+function exec() {
+  client.get('key', complete, cancel);
+}
+
+
+function run() {
+  r = 0;
+  e = 0;
+  t = Date.now();
+  mem = 0;
+  count += step;
+
+  if (count / step === 10) {
+    step *= 10;
+  }
+
+  for (var i = 0; i < count; i += 1) {
+    exec();
+  }
+}
+
+
+client.set('key', data, function() {
+  console.log('[REDIS-POLINA]');
+  console.log('   R   |   E   |   T    |   M  ');
+  run();
+}, console.error);
+
+
+
+
